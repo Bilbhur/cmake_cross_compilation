10843a84cfccc1a048ba7ff9e4af78a4481e3edb
alexandremajda <majda.a@outlook.com>
2020-12-17 18:31:17 +0100

    [Feature] Cross compiling hello world

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <conio.j
+
+int main() {
+    std::cout << "Hello, World! What's your name ?" << std::endl;
+    std::string test;
+    std::cin >> test;
+
+    std::cout << "Glad to meet you " << test << std::endl;
+    return 0;
+}
